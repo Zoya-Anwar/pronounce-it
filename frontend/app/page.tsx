842f7bae1ea8988b150680923498e7ef37d4acff
syzwina <nor.syazwina.ahmad.fazrul@gmail.com>
2023-10-28 17:37:39 +0100

    Hidden css and language level cards

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,17 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { useState } from "react";
 
 export default function Home() {
+  const [show, setShow] = useState(true);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-2xl font-bold pb-10">some random app</h1>
-      <Button className="font-bold w-24">Start</Button>
+      <h1 className="text-2xl font-bold pb-10">apprenons le français</h1>
+      {show ? <Button className="font-bold m-2 w-24" onClick={() => setShow(!show)}> Start </Button> : null}
+      {show ? null : <Card><CardContent className="font-bold p-2 m-2 w-50">Beginner</CardContent></Card>}
+      {show ? null : <Card><CardContent className="font-bold p-2 m-2 w-50">Intermediate</CardContent></Card>}
+      {show ? null : <Card><CardContent className="font-bold p-2 m-2 w-50">Advanced</CardContent></Card>}
     </main>
   );
 }
