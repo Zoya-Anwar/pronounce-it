6c5f8c01cfa61839e9662054985e856609e47869
syzwina <nor.syazwina.ahmad.fazrul@gmail.com>
2023-10-28 17:46:55 +0100

    Layout cards horizontally

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,10 +8,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-2xl font-bold pb-10">apprenons le français</h1>
+      <div className="flex h-5 items-center space-x-4 text-sm">
       {show ? <Button className="font-bold m-2 w-24" onClick={() => setShow(!show)}> Start </Button> : null}
-      {show ? null : <Card className="m-2"><CardContent className="font-bold p-2 m-2 w-50">Beginner</CardContent></Card>}
-      {show ? null : <Card className="m-2"><CardContent className="font-bold p-2 m-2 w-50">Intermediate</CardContent></Card>}
-      {show ? null : <Card className="m-2"><CardContent className="font-bold p-2 m-2 w-50">Advanced</CardContent></Card>}
+      {show ? null : <Card className="m-2"><CardContent className="font-bold p-2 m-2 w-55">Beginner</CardContent></Card>}
+      {show ? null : <Card className="m-2"><CardContent className="font-bold p-2 m-2 w-55">Intermediate</CardContent></Card>}
+      {show ? null : <Card className="m-2"><CardContent className="font-bold p-2 m-2 w-55">Advanced</CardContent></Card>}
+      </div>
     </main>
   );
 }
