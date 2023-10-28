3c1d28386a4bd03082dfea2ec245b53e2e536618
Dhruv <59387969+druvdub@users.noreply.github.com>
2023-10-28 19:41:45 +0100

    Add interceptor

--- frontend/lib/interceptor.ts
+++ frontend/lib/interceptor.ts
@@ -1,7 +1,23 @@
-import axios from "axios";
+import axios, { type AxiosError } from "axios";
 
 // Create an axios instance
 
 const axiosInstance = axios.create({
-  baseURL: "",
+  baseURL: "http://localhost:8080/api", // placeholder for backend url
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    // if error, return error message and status code
+    if (error.response) {
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error);
+  }
+);
