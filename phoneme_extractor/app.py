63ebe05bf6c03397914f0e4b56569828931bf285
Gwydion-Smit <67271589+Gwydion-Smit@users.noreply.github.com>
2023-10-29 01:03:14 +0100

    Add template for level and audio endpoints

--- phoneme_extractor/app.py
+++ phoneme_extractor/app.py
@@ -1,6 +1,7 @@
 # app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+import base64
 
 # gives words in "word_to_transcribe", "phonetic_transcription" form
 from phonetic_words import get_french_phonetic_words, get_english_phonetic_words
@@ -22,18 +23,35 @@ def _find_next_id():
 
 @app.get("/api/word")
 def get_word():
-    return jsonify(words)
+    return jsonify(words), 200
 
 @app.get("/api/find/english_phoneme/{ipa_letter}")
 def find_english_phoneme(ipa_letter: str, request: Request):
     if request.is_json:
         english_word, english_phonetic_transcription = get_english_phonetic_words(ipa_letter)
-        return english_word, 201
+        return english_word, 200
     return {"error": "Request must be JSON"}, 415
 
 @app.get("/api/find/french_phoneme/{ipa_letter}")
 def find_french_phoneme(ipa_letter: str, request: Request):
     if request.is_json:
         french_word, french_phonetic_transcription = get_french_phonetic_words(ipa_letter)
-        return french_word, 201
+        return french_word, 200
     return {"error": "Request must be JSON"}, 415
+
+@app.post("/api/level")
+def set_level(request: Request):
+    if request.is_json:
+        level = request.json["level"]
+        # set_level if time
+        return 200
+    return {"error": "Request must be JSON"}, 415
+
+@app.post("/api/submit_audio")
+def check_audio(request: Request):
+    if request.is_json:
+        b64 = request.json["audiob64"]
+        wav = base64.decodebytes(b64)
+        # submit wav file for checking
+        return "test", 200
+    return {"error": "Something went wrong :("}, 415
