c360a3f2e51e1ef38773870eb921a16bb86e52f0
syzwina <nor.syazwina.ahmad.fazrul@gmail.com>
2023-10-29 01:06:50 +0000

    Re-added gwyd's level and audio code

--- phoneme_extractor/app/main.py
+++ phoneme_extractor/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from starlette.requests import Request
+import base64
 from app.internal.phonetic_words import get_french_phonetic_words, get_english_phonetic_words
 
 app = FastAPI()
@@ -8,13 +9,30 @@ app = FastAPI()
 @app.get("/api/find/french_phoneme/{ipa_letter}")
 async def find_french_phoneme(ipa_letter: str, request: Request):
     french_word, french_phonetic_transcription = get_french_phonetic_words(ipa_letter)
-    return {"french_word": french_word, "french_phonetic_transcription": french_phonetic_transcription}
+    return {"french_word": french_word, "french_phonetic_transcription": french_phonetic_transcription}, 200
 
 # Define the API endpoint
 @app.get("/api/find/english_phoneme/{ipa_letter}")
 async def find_english_phoneme(ipa_letter: str, request: Request):
     english_word, english_phonetic_transcription = get_english_phonetic_words(ipa_letter)
-    return {"english_word": english_word, "english_phonetic_transcription": english_phonetic_transcription}
+    return {"english_word": english_word, "english_phonetic_transcription": english_phonetic_transcription}, 200
+
+@app.post("/api/level")
+def set_level(request: Request):
+    if request.is_json:
+        level = request.json["level"]
+        # set_level if time
+        return 200
+    return {"error": "Request must be JSON"}, 415
+
+@app.post("/api/submit_audio")
+def check_audio(request: Request):
+    if request.is_json:
+        b64 = request.json["audiob64"]
+        wav = base64.decodebytes(b64)
+        # submit wav file for checking
+        return "test", 200
+    return {"error": "Something went wrong :("}, 415
 
 # Run the FastAPI application
 if __name__ == "__main__":
