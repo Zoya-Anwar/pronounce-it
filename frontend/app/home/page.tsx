06518ed2c62f0537e637989e3316cc76732b01d9
Dhruv <59387969+druvdub@users.noreply.github.com>
2023-10-29 04:31:49 +0000

    Add post mutation query

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -5,17 +5,19 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { ArrowRightSquare, MoveRight } from "lucide-react";
 import { useState, useEffect } from "react";
-import { useGetWordQuery, ResponseData } from "@/hooks/query/use-get-word-query";
+import {
+  useGetWordQuery,
+  ResponseData,
+} from "@/hooks/query/use-get-word-query";
 
 export default function Home() {
-  const [returnString, setReturnString] = useState("");
-  // const [onLoad, setOnLoad] = useState(true);
+  const [returnString, setReturnString] = useState<JSX.Element | null>(null);
   const [testWord, setTestWord] = useState("");
 
   const wordQuery = useGetWordQuery({
     onSuccess(data: ResponseData) {
       console.log("success");
-      setReturnString("");
+      setReturnString(null);
       setTestWord(data.word);
     },
   });
@@ -23,30 +25,24 @@ export default function Home() {
   useEffect(() => {
     //CALL MUTATE FUNC (or get api endpoint) to get a word;
     wordQuery.refetch();
-  }, []);
+  }, [wordQuery]);
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <Card className="min-w-[40vw] min-h-[20vh] flex justify-center align-middle items-center m-2">
         <CardContent className="font-black text-7xl text-center">
-          {testWord}
+          {returnString !== null ? returnString : testWord}
         </CardContent>
       </Card>
       <div
         className={`flex flex-row flex-wrap content-center w-[40vw] ${
-          returnString !== "" ? "justify-between" : "justify-center"
+          returnString !== null ? "justify-between" : "justify-center"
         }`}
       >
         <MicButton returningString={setReturnString} word={testWord} />
-        {returnString!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! !==
-        "" ? (
+        {returnString!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! !==
+        null ? (
           <div>
-            {/* <Card className="min-w-[40vw] min-h-[60vh] flex justify-center align-middle items-center">
-            {" "}
-            <CardContent className="font-black text-9xl text-center">
-              {returnString}
-            </CardContent>
-          </Card> */}
             <Button
               variant={"link"}
               className="m-2 hover:text-purple-800"
--- frontend/components/mic_button.tsx
+++ frontend/components/mic_button.tsx
@@ -1,13 +1,47 @@
 import { Pause, Play } from "lucide-react";
 import { Button } from "./ui/button";
-import { useState, useRef } from "react";
-// import mutation post
+import { useState, useRef, use } from "react";
+import { usePostAudioMutation } from "@/hooks/mutations/use-post-audio-mutation";
 
 const MicButton = (props: any) => {
   const [usingMic, setMic] = useState(false);
   let mediaRecorder = useRef<MediaRecorder | null>(null);
   const [audioChunks, setAudioChunks] = useState<any>([]);
   const [audio, setAudio] = useState<string>("");
+  const [outputResult, setOutputResult] = useState<JSX.Element | null>(null);
+
+  const useAudioMutation = usePostAudioMutation({
+    onSuccess(data) {
+      console.log("success");
+      console.log(data);
+      // check the result in data, if it has delimiters [], then color the words between the delimiters red and the rest black
+
+      // use regex to find the delimiters and then split the string into an array of strings
+      const array = data.result.split(/(\[.*?\])/g);
+
+      // map over the array and if the string starts with [, then color it red remove the brackets and combine the array back into a string
+      const result = (
+        <>
+          {array.map((item: string, index: number) => {
+            if (item.startsWith("[")) {
+              <span key={index} className="text-red-500">
+                {item.replace("[", "").replace("]", "")}
+              </span>;
+            } else {
+              <span key={index}>{item}</span>;
+            }
+          })}
+        </>
+      );
+
+      // set the result to the result state
+      setOutputResult(result);
+    },
+    onError(error) {
+      console.log("error");
+      console.log(error);
+    },
+  });
 
   async function doStuff() {
     console.log(usingMic);
@@ -51,8 +85,12 @@ const MicButton = (props: any) => {
       reader.onloadend = function () {
         var base64data = reader.result;
         console.log(base64data);
-        props.returningString("WILL BE MUTATED!");
-        // MUTATE HERE
+        props.returningString(outputResult);
+
+        // if base64data is not null, then call the mutation
+        if (base64data) {
+          useAudioMutation.mutate({ audio: base64data.toString() });
+        }
       };
     };
   }
