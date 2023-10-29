54c0112fcbfcd1dc83560d0dc163eda88aa84ad3
Zoya Anwar <zoyaanwar@Zoyas-MacBook-Air.local>
2023-10-29 04:44:24 +0000

    Add generative AI for sentence generation

--- phoneme_extractor/app/main.py
+++ phoneme_extractor/app/main.py
@@ -4,6 +4,7 @@ import base64
 from app.internal.phonetic_words import get_french_phonetic_words, get_english_phonetic_words
 from app.internal.audio.rate_pronounciation import phoneme_similarity
 from app.database.db import get_top_10_lowest_phoneme, get_user_level_value
+from app.generate_sentences.py import generate_sentence_french
 import random
 
 app = FastAPI()
@@ -25,8 +26,17 @@ async def get_new_word(request: Request):
     if request.is_json:
         username = request["username"]
         threshold = get_user_level_value(username)
-        worst_phonemes = filter(lambda x: x["score"] < threshold ,get_top_10_lowest_phoneme(username))
+        worst_phonemes = filter(lambda x: x["score"] < threshold, get_top_10_lowest_phoneme(username))
         return find_french_phoneme(random.choice(worst_phonemes), None)
+    return {"error": "Meep noises"}, 415
+
+@app.get("/api/find/new_sentence")
+async def get_new_sentence(request: Request):
+    if request.is_json:
+        username = request["username"]
+        threshold = get_user_level_value(username)
+        worst_phonemes = filter(lambda x: x["score"] < threshold, get_top_10_lowest_phoneme(username))
+        return generate_sentence_french(random.choice(worst_phonemes), None)
     return {"error": "Meep noises"}, 415
 
 
@@ -43,7 +53,7 @@ async def check_audio(request: Request):
     if request.is_json:
         b64 = request.json["audiob64"]
         wav = base64.decodebytes(b64)
-        with open("./internal/output.wav", "rb") as file:
+        with open("internal/audio/output.wav", "rb") as file:
             file.write(wav)
         # Perform test and then save to db
         return "test", 200
