17764afb46ed846c1533d35516e9daff3943e2ea
Gwydion-Smit <67271589+Gwydion-Smit@users.noreply.github.com>
2023-10-29 09:49:24 +0000

    Error

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/hooks/query/use-get-word-query";
 
 export default function Home() {
-  const [returnString, setReturnString] = useState<JSX.Element | null>(null);
+  const [returnString, setReturnString] = useState<string | null>(null);
   const [testWord, setTestWord] = useState("");
   const [firstWord, setFirstWord] = useState(true);
 
@@ -37,7 +37,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <Card className="min-w-[40vw] min-h-[20vh] flex justify-center align-middle items-center m-2">
         <CardContent className="font-black text-7xl text-center">
-          {returnString !== null ? returnString : testWord}
+          {returnString !== null ? <div dangerouslySetInnerHTML={{__html: returnString}}></div> : testWord}
         </CardContent>
       </Card>
       <div
@@ -49,6 +49,11 @@ export default function Home() {
         {returnString!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! !==
         null ? (
           <div>
+            {/* <Card className="min-w-[40vw] min-h-[20vh] flex justify-center align-middle items-center m-2">
+        <CardContent className="font-black text-7xl text-center">
+          {returnString}
+        </CardContent>
+      </Card> */}
             <Button
               variant={"link"}
               className="m-2 hover:text-purple-800"
--- frontend/components/mic_button.tsx
+++ frontend/components/mic_button.tsx
@@ -1,6 +1,6 @@
 import { Pause, Play } from "lucide-react";
 import { Button } from "./ui/button";
-import { useState, useRef, use } from "react";
+import { useState, useRef, use, Fragment } from "react";
 import { usePostAudioMutation } from "@/hooks/mutations/use-post-audio-mutation";
 
 const MicButton = (props: any) => {
@@ -8,34 +8,37 @@ const MicButton = (props: any) => {
   let mediaRecorder = useRef<MediaRecorder | null>(null);
   const [audioChunks, setAudioChunks] = useState<any>([]);
   const [audio, setAudio] = useState<string>("");
-  const [outputResult, setOutputResult] = useState<JSX.Element | null>(null);
 
   const useAudioMutation = usePostAudioMutation({
     onSuccess(data) {
       console.log("success");
-      console.log(data);
+      console.log(data.result);
       // check the result in data, if it has delimiters [], then color the words between the delimiters red and the rest black
 
       // use regex to find the delimiters and then split the string into an array of strings
       const array = data.result.split(/(\[.*?\])/g);
 
-      // map over the array and if the string starts with [, then color it red remove the brackets and combine the array back into a string
-      const result = (
-        <>
-          {array.map((item: string, index: number) => {
-            if (item.startsWith("[")) {
-              <span key={index} className="text-red-500">
-                {item.replace("[", "").replace("]", "")}
-              </span>;
-            } else {
-              <span key={index}>{item}</span>;
-            }
-          })}
-        </>
-      );
+      console.log(array);
 
-      // set the result to the result state
-      setOutputResult(result);
+      // map over the array and if the string starts with [, then color it red remove the brackets and combine the array back into a string
+
+      let result = "";
+
+      array.map((item: string, index: number) => {
+            if (item.startsWith("[")) {
+              result += `<span key=${index} class="text-red-500 m-0">
+                ${item.replace("[", "").replace("]", "").trim()}
+              </span>`
+            } else {
+              result += `<span class="m-0" key=${index}>${item.trim()}</span>`
+            }
+          });
+
+      // const result = array.map(str => `<span class="text-red-800">${str}</span>`).join('');
+
+      console.log(result);
+      props.returningString(result);
+
     },
     onError(error) {
       console.log("error");
@@ -82,9 +85,9 @@ const MicButton = (props: any) => {
         data.append('file', audioBlob)
         data.append('word', props.word)
 
-        props.returningString(outputResult);
         // if base64data is not null, then call the mutation
         useAudioMutation.mutate(data);
+    
     };
   }
 
